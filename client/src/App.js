70512002bf7e2120284452c989949b3c1306397e
Divya <divyasrigolla727@gmail.com>
2026-03-21 21:05:51 +0530

    Fix App.js eslint

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext, useContext } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
